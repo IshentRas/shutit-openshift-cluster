a2ab764ec79f6883c3f5a9d011ff2a823e8731ef
Ian Miell <ian.miell@gmail.com>
2017-02-06 17:44:34 +0000

    reinstate

--- run_tests.sh
+++ run_tests.sh
@@ -10,24 +10,25 @@ then
 	echo "Must have shutit on path, eg export PATH=$PATH:/path/to/shutit_dir"
 	exit 1
 fi
-#for ose_major_version in 3.3 3.2
-#do
-#	for test_dir in $(cd tests && find * -type d && cd - > /dev/null)
-#	do
-#		$SHUTIT build \
-#			--echo -d bash \
-#			-m shutit-library/vagrant:shutit-library/virtualbox \
-#			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster test_config_dir ${test_dir} \
-#			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_major_version ${ose_major_version} \
-#			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_yum_cookbook_version latest \
-#			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_iptables_cookbook_version latest \
-#			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_selinux_policy_cookbook_version latest \
-#			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_compat_resource_cookbook_version latest \
-#			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_version 12.16.42-1 \
-#            "$@"
-#		./destroy_vms.sh
-#	done
-#done
+
+for ose_major_version in 3.3 3.2
+do
+	for test_dir in $(cd tests && find * -type d && cd - > /dev/null)
+	do
+		$SHUTIT build \
+			--echo -d bash \
+			-m shutit-library/vagrant:shutit-library/virtualbox \
+			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster test_config_dir ${test_dir} \
+			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_major_version ${ose_major_version} \
+			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_yum_cookbook_version latest \
+			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_iptables_cookbook_version latest \
+			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_selinux_policy_cookbook_version latest \
+			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_compat_resource_cookbook_version latest \
+			-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_version 12.16.42-1 \
+            "$@"
+		./destroy_vms.sh
+	done
+done
 
 # $WORK-specific
 
